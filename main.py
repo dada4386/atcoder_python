933fb7f60aaa4d9d8d6a13bc2c412ce01214b8ed
Daiki Murayama <dai_fs6gwws4386@outlook.jp>
2022-02-20 17:11:24 +0900

    Add contest template for atcoder

---
+++ main.py
@@ -0,0 +1,13 @@
+import math
+# from decimal import *
+# import itertools
+import sys
+import resource
+
+sys.setrecursionlimit(10 ** 9)
+resource.setrlimit(resource.RLIMIT_STACK, (-1, -1))
+ 
+def main():
+    pass
+ 
+main()
