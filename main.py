bebb3fa2f3a5d7a3c5b2b90eb900761997715b9f
Daiki Murayama <dai_fs6gwws4386@outlook.jp>
2022-02-21 22:27:35 +0900

    Fix template file

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import math
 # from decimal import *
+# from itertools import combinations
 # import itertools
 import sys
-import resource
-
 sys.setrecursionlimit(10 ** 9)
-resource.setrlimit(resource.RLIMIT_STACK, (-1, -1))
- 
+
+def chmax(a, i, x):
+    if x > a[i]:
+        a[i] = x
+        return True
+    return False
+
+def chmin(a, i, x):
+    if x < a[i]:
+        a[i] = x
+        return True
+    return False
+
 def main():
     pass
- 
+
 main()
